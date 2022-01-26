6aad0e89f8f467f6096a7f7c2f5accdae2e1c7b2
softbalanced <89533513+softbalanced@users.noreply.github.com>
2022-01-26 08:22:48 +0100

    Update validation.h

--- src/validation.h
+++ src/validation.h
@@ -59,12 +59,12 @@ static const bool DEFAULT_WHITELISTFORCERELAY = true;
 static const unsigned int DEFAULT_MIN_RELAY_TX_FEE = 1000;
 
 //! -maxtxfee default
-//static const CAmount DEFAULT_TRANSACTION_MAXFEE = 0.00001 * COIN;
-static const CAmount DEFAULT_TRANSACTION_MAXFEE = 0.1 * COIN;
+static const CAmount DEFAULT_TRANSACTION_MAXFEE = 0.00001 * COIN;
+//static const CAmount DEFAULT_TRANSACTION_MAXFEE = 0.1 * COIN;
 
 //! Discourage users to set fees higher than this amount (in Cent) per kB
-//static const CAmount HIGH_TX_FEE_PER_KB = 0.00001 * COIN;
-static const CAmount HIGH_TX_FEE_PER_KB = 0.01 * COIN;
+static const CAmount HIGH_TX_FEE_PER_KB = 0.000001 * COIN;
+//static const CAmount HIGH_TX_FEE_PER_KB = 0.01 * COIN;
 
 //! -maxtxfee will warn if called with a higher fee than this amount (in Cent)
 static const CAmount HIGH_MAX_TX_FEE = 100 * HIGH_TX_FEE_PER_KB;
